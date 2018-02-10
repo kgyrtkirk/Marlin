b8b666514d5be7719b9fbbabe427644b0a10520b
Zoltan Haindrich <kirk@rxd.hu>
2018-02-10 16:23:16 +0100

    reduce accelration a lot!

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -543,7 +543,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 400, 400, 8, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 400, 8, 30 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -551,7 +551,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      {750,750,75,100}
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -562,8 +562,10 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          400     // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
+//#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
+//#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_RETRACT_ACCELERATION  750
+#define DEFAULT_TRAVEL_ACCELERATION   75
 
 /**
  * Default Jerk (mm/s)
@@ -576,7 +578,7 @@
 #define DEFAULT_XJERK                 10.0
 #define DEFAULT_YJERK                 10.0
 #define DEFAULT_ZJERK                  0.3
-#define DEFAULT_EJERK                  5.0
+#define DEFAULT_EJERK                  0.10
 
 //===========================================================================
 //============================= Z Probe Options =============================
